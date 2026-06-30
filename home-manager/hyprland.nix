ea36466921e3f888ec6a9fbed0835fd58d21148d
Annabelle Chevreau <annabelle.chevreau@gmail.com>
2026-06-30 22:44:08 +0200

    feat: update hyprland config

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -3,7 +3,10 @@
   home.packages = with pkgs; [
     kitty
     wofi
-    waybar
+    wl-clipboard
+    wlogout
+    wlr-randr
+    hyprshot
   ];
 
   wayland.windowManager.hyprland = {
@@ -24,11 +27,45 @@
         "WLR_RENDERER_ALLOW_SOFTWARE,1"
       ];
 
-      monitor = [ ",preferred,auto,1" ];
+      monitor = [
+        ",preferred,auto,1"
+      ];
+
+      general = {
+        border_size = 2;
+        gaps_in = 4;
+        gaps_out = 6;
+        "col.inactive_border" = "rgb(150457)";
+        "col.active_border" = "rgb(C85CE7)";
+        resize_on_border = true;
+      };
+
+      decoration = {
+        rounding = 10;
+      };
+
+      animations = {
+        enabled = true;
+        animation = [
+          "windowsMove, 1, 5, default, slide"
+        ];
+      };
 
       input = {
         kb_layout = "us";
-        kb_variant = "alt-intl";
+        kb_variant = "intl";
+        touchpad = {
+          natural_scroll = true;
+          scroll_factor = 0.6;
+          "tap-to-click" = true;
+          "tap-and-drag" = true;
+        };
+        follow_mouse = 1; # Cursor movement will always change focus to the window under the cursor.
+        numlock_by_default = true;
+      };
+
+      misc = {
+        disable_hyprland_logo = true;
       };
 
       bind = [
@@ -36,6 +73,34 @@
         "$mod SHIFT, Q, killactive"
         "$mod, Space, exec, wofi --show drun"
         "$mod SHIFT, E, exit"
+
+        ", PRINT, exec, hyprshot -m region --freeze --output-folder ~/Pictures/"
+        "$mod SHIFT, S, exec,hyprshot -m region --freeze --clipboard"
+
+        ", XF86MonBrightnessUp, exec, light -A 5"
+        ", XF86MonBrightnessDown, exec, light -U 5"
+
+        ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%+"
+        ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%-"
+        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+
+        # Move focus
+        "$mod, H, movefocus, l"
+        "$mod, J, movefocus, d"
+        "$mod, K, movefocus, u"
+        "$mod, L, movefocus, r"
+
+        # Move windows
+        "$mod SHIFT, H, movewindow, l"
+        "$mod SHIFT, J, movewindow, d"
+        "$mod SHIFT, K, movewindow, u"
+        "$mod SHIFT, L, movewindow, r"
+
+        # Resize windows
+        "$mod CTRL, H, resizeactive, -10 0"
+        "$mod CTRL, J, resizeactive, 0 10"
+        "$mod CTRL, K, resizeactive, 0 -10"
+        "$mod CTRL, L, resizeactive, 10 0"
       ]
       ++ (
         # workspaces
@@ -57,6 +122,11 @@
           ) 10
         )
       );
+
+      bindm = [
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+      ];
 
       exec-once = [
         "waybar"
