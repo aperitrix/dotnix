78e62cfc581ab1febb7a39fe4f8c712c96bc5e19
Annabelle Chevreau <annabelle.chevreau@gmail.com>
2026-09-06 11:00:26 +0200

    clean: remove hyprland

--- flake.nix
+++ flake.nix
@@ -7,11 +7,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    hyprland = {
-      url = "github:hyprwm/Hyprland";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     nixvim.url = "github:nix-community/nixvim";
 
     lazygit.url = "github:jesseduffield/lazygit";
